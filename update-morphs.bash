f07653602e1061530af733a9ab3bbf201aed667a
Ilnar Salimzyan <ilnar@selimcan.org>
2012-04-17 19:00:39 +0000

    Just a simple script for getting monolingual stuff from other directories

---
+++ update-morphs.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp ../apertium-kaz/apertium-kaz.kaz.lexc apertium-kaz-tat.kaz.lexc
+cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol
+cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-tat.kaz-tat.rlx
+
+cp ~/src/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt.lexc apertium-kaz-tat.tat.lexc
+cp ~/src/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt.twol apertium-kaz-tat.tat.twol
+cp ~/src/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt-ba.rlx apertium-kaz-tat.tat-kaz.rlx
+
+exit 0
+
+
