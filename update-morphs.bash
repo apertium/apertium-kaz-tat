f6fc5a904766108e8a361bc82706c37814ca69e7
Francis M. Tyers <ftyers@prompsit.com>
2012-04-21 19:18:50 +0000

    trim lexicons

--- update-morphs.bash
+++ update-morphs.bash
@@ -4,9 +4,9 @@ cp ../apertium-kaz/apertium-kaz.kaz.lexc apertium-kaz-tat.kaz.lexc
 cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol
 cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-tat.kaz-tat.rlx
 
-cp ~/src/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt.lexc apertium-kaz-tat.tat.lexc
-cp ~/src/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt.twol apertium-kaz-tat.tat.twol
-cp ~/src/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt-ba.rlx apertium-kaz-tat.tat-kaz.rlx
+cp ~/source/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt.lexc apertium-kaz-tat.tat.lexc
+cp ~/source/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt.twol apertium-kaz-tat.tat.twol
+cp ~/source/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt-ba.rlx apertium-kaz-tat.tat-kaz.rlx
 
 exit 0
 
