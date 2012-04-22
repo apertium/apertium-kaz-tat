6bd895685c990d23064e052236634e2b9281279f
Ilnar Salimzyan <ilnar@selimcan.org>
2012-04-22 02:24:20 +0000

    get tatar stuff from incubator/a-tat, not nursery/a-tt-ba

--- update-morphs.bash
+++ update-morphs.bash
@@ -4,9 +4,9 @@ cp ../apertium-kaz/apertium-kaz.kaz.lexc apertium-kaz-tat.kaz.lexc
 cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol
 cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-tat.kaz-tat.rlx
 
-cp ~/source/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt.lexc apertium-kaz-tat.tat.lexc
-cp ~/source/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt.twol apertium-kaz-tat.tat.twol
-cp ~/source/apertium/nursery/apertium-tt-ba/apertium-tt-ba.tt-ba.rlx apertium-kaz-tat.tat-kaz.rlx
+cp ../apertium-tat/apertium-tat.tat.lexc apertium-kaz-tat.tat.lexc
+cp ../apertium-tat/apertium-tat.tat.twol apertium-kaz-tat.tat.twol
+cp ../apertium-tat/apertium-tat.tat.rlx apertium-kaz-tat.tat-kaz.rlx
 
 exit 0
 
