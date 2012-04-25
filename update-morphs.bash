9fafd6fd927637e055899cecf5c69c334a6ede30
Ilnar Salimzyan <ilnar@selimcan.org>
2012-04-25 15:16:27 +0000

    a script for updating monolingual stuff for the lazy selimcan

--- update-morphs.bash
+++ update-morphs.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cp ../apertium-kaz/apertium-kaz.kaz.lexc apertium-kaz-tat.kaz.lexc
+python3 ~/src/apertium/trunk/apertium-tools/trim-lexc.py apertium-kaz-tat.kaz-tat.dix ../apertium-kaz/apertium-kaz.kaz.lexc ../apertium-tat/apertium-tat.tat.lexc
+
+cp /tmp/apertium-kaz.kaz.lexc.trimmed apertium-kaz-tat.kaz.lexc
+cp /tmp/apertium-tat.tat.lexc.trimmed apertium-kaz-tat.tat.lexc
+
 cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol
 cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-tat.kaz-tat.rlx
 
