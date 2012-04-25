39532b96822aa7320690dcae2e9aa55e280a557b
Ilnar Salimzyan <ilnar@selimcan.org>
2012-04-25 22:20:17 +0000

    a fix in the update-morphs.bash

--- update-morphs.bash
+++ update-morphs.bash
@@ -8,7 +8,6 @@ cp /tmp/apertium-tat.tat.lexc.trimmed apertium-kaz-tat.tat.lexc
 cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol
 cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-tat.kaz-tat.rlx
 
-cp ../apertium-tat/apertium-tat.tat.lexc apertium-kaz-tat.tat.lexc
 cp ../apertium-tat/apertium-tat.tat.twol apertium-kaz-tat.tat.twol
 cp ../apertium-tat/apertium-tat.tat.rlx apertium-kaz-tat.tat-kaz.rlx
 
