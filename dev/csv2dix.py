b0a271ef8220a3a00c8d2dd92166c0698823ab78
Francis M. Tyers <ftyers@prompsit.com>
2012-06-20 22:26:48 +0000

    updat

--- dev/csv2dix.py
+++ dev/csv2dix.py
@@ -34,11 +34,15 @@ ttable = {
 	'y':'й',
 	'z':'з',
 	'ı':'ы',
+	'\'':'ь',
 	'h̦':'һ'
 };
 
 # Repair errors in Kazakh transliteration
 kazfix = {
+	'йу':'ю',
+	'йа':'я',
+	'йе':'е',
 	'ö':'ө',
 	'ü':'ү',
 	'ğ':'ғ',
@@ -53,20 +57,21 @@ tatfix = {
 	'ö':'ө',
 	'ü':'ү',
 	'ğ':'г',
-	'q':'к',
-	'\'':'ь'
+	'q':'к'
 };
 
 # Repair errors in Russian transliteration
 rusfix = {
+	'рьа':'ря',
 	'ийе':'ие',
 	'ойе':'ое',
+	'ü':'ю',
+	'ö':'ё',
 	'йу':'ю',
 	'йа':'я',
 	'йе':'е',
 	'тс':'ц',
-	'һ':'х',
-	'\'':'ь'
+	'һ':'х'
 };
 
 def t(s, v): #{
@@ -91,11 +96,38 @@ def t(s, v): #{
 	return out;
 #}
 
+def guess_pos(s): #{
+	pos = 'unk';
+
+	end_n = ['ность', 'ство', 'ция', 'ние'];
+	end_a = ['ный', 'ний'];	
+	end_v = ['ить', 'ать'];	
+
+	for i in end_n: #{
+		if s.count(i) > 0: #{
+			pos = 'n';
+		#}
+	#}
+
+	for i in end_a: #{
+		if s.count(i) > 0: #{
+			pos = 'adj';
+		#}
+	#}
+
+	for i in end_v: #{
+		if s.count(i) > 0: #{
+			pos = 'v';
+		#}
+	#}
+
+	return pos;
+#}
+
 #jen̦ilüv,cin̦ilü,snova
 #jergilikti,"bu urındağı","zemnoye t'agoteniye"
 #"sıkak (ädebiyet)","açı kölü",mestnıy
 #jer,cir,pobedit'
-
 
 for line in open(sys.argv[1]).readlines(): #{
 	if line.count(',') < 1: #{
@@ -108,5 +140,7 @@ for line in open(sys.argv[1]).readlines(): #{
 	tat = t(row[1], 'tat');
 	rus = t(row[2], 'rus');
 
-	print('<e c="%s"><p><l>%s<s n="unk"/></l><r>%s<s n="unk"/></r></p></e>' % (rus, kaz, tat));
+	pos = guess_pos(rus);	
+
+	print('<e c="%s"><p><l>%s<s n="%s"/></l><r>%s<s n="%s"/></r></p></e>' % (rus, kaz, pos, tat, pos));
 #}
