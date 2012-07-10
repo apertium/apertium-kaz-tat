9d4e8ea1e7e143c21366a158928ad862d6ab0460
Ilnar Salimzyan <ilnar@selimcan.org>
2012-07-10 12:17:46 +0000

    more words; typos in the story. That's it -working wiht a corpus

--- testcorpus_kaz-tat.sh
+++ testcorpus_kaz-tat.sh
@@ -5,13 +5,13 @@
 # See http://wiki.apertium.org/wiki/Corpus_test
 #
 # To make 'original' translation, type this
-#make && cat corpa/kaz.crp.txt | apertium -d . kaz-tat > origina_traduko.txt &&
+#make && cat corpa/kaz.crp.txt | apertium -d . kaz-tat > corpa/origina_traduko.txt &&
 
-cat corpa/kaz.crp.txt | apertium -d . kaz-tat > nova_traduko.txt &&
+cat corpa/kaz.crp.txt | apertium -d . kaz-tat > corpa/nova_traduko.txt &&
 
-diff -w origina_traduko.txt nova_traduko.txt | grep '^[<>]' > /tmp/crpdiff.txt && 
+diff -w corpa/origina_traduko.txt corpa/nova_traduko.txt | grep '^[<>]' > /tmp/crpdiff.txt && 
   for i in `cut -c3-8 /tmp/crpdiff.txt | sort -un`; do 
     echo  --- $i ---; grep "^ *$i\." corpa/kaz.crp.txt; grep "^. *$i\." /tmp/crpdiff.txt; 
   done | less
 
-#diff -U1 origina_traduko.txt nova_traduko.txt | dwdiff -c --diff-input
+#diff -U1 corpa/origina_traduko.txt /corpa/nova_traduko.txt | dwdiff -c --diff-input
