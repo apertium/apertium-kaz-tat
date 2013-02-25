1ca712a94487d58f0153a5993833a73b899872a8
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-02-25 21:44:35 +0000

    update-morphs script updates

--- update-morphs.bash
+++ update-morphs.bash
@@ -2,16 +2,16 @@
 
 #assuming that you have the whole apertium tree in your source dir. and you are in kaz-tat directory
 
-python3 ../../trunk/apertium-tools/trim-lexc.py apertium-kaz-tat.kaz-tat.dix ../apertium-kaz/apertium-kaz.kaz.lexc ../apertium-tat/apertium-tat.tat.lexc
+python3 ../../trunk/apertium-tools/trim-lexc.py apertium-kaz-tat.kaz-tat.dix ../../incubator/apertium-kaz/apertium-kaz.kaz.lexc ../../incubator/apertium-tat/apertium-tat.tat.lexc
 
 cp /tmp/apertium-kaz.kaz.lexc.trimmed apertium-kaz-tat.kaz.lexc
 cp /tmp/apertium-tat.tat.lexc.trimmed apertium-kaz-tat.tat.lexc
 
-cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol
-cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-tat.kaz-tat.rlx
+cp ../../incubator/apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol
+cp ../../incubator/apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-tat.kaz-tat.rlx
 
-cp ../apertium-tat/apertium-tat.tat.twol apertium-kaz-tat.tat.twol
-cp ../apertium-tat/apertium-tat.tat.rlx apertium-kaz-tat.tat-kaz.rlx
+cp ../../incubator/apertium-tat/apertium-tat.tat.twol apertium-kaz-tat.tat.twol
+cp ../../incubator/apertium-tat/apertium-tat.tat.rlx apertium-kaz-tat.tat-kaz.rlx
 
 exit 0
 
