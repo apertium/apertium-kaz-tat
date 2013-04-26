a7b7cb04b8c9005691dc46812840d373c0b5de75
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-04-26 16:35:48 +0000

    update-morphs script should now REALLY check for whether there are changes to the twol files, and if not, doesn't update them

--- update-morphs.bash
+++ update-morphs.bash
@@ -7,13 +7,13 @@ python3 ../../trunk/apertium-tools/trim-lexc.py apertium-kaz-tat.kaz-tat.dix ../
 cp /tmp/apertium-kaz.kaz.lexc.trimmed apertium-kaz-tat.kaz.lexc
 cp /tmp/apertium-tat.tat.lexc.trimmed apertium-kaz-tat.tat.lexc
 
-DIFF=$(diff ../incubator/apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol)
+DIFF=$(diff ../../incubator/apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol)
 if [ "$DIFF" != "" ]; then
 	cp ../../incubator/apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol
 fi;
 cp ../../incubator/apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-tat.kaz-tat.rlx
 
-DIFF=$(diff ../incubator/apertium-tat/apertium-tat.tat.twol apertium-kaz-tat.tat.twol)
+DIFF=$(diff ../../incubator/apertium-tat/apertium-tat.tat.twol apertium-kaz-tat.tat.twol)
 if [ "$DIFF" != "" ]; then
 	cp ../../incubator/apertium-tat/apertium-tat.tat.twol apertium-kaz-tat.tat.twol
 fi;
