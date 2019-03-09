ce55b37c4525c8d6e2cc8a379c719df75b7b1489
Ilnar Salimzianov <ilnar@selimcan.org>
2019-03-09 15:08:02 +0300

    add in interface script to assist me on issue apertium/apertium-kaz#11

---
+++ main.rkt
@@ -0,0 +1,43 @@
+#lang racket
+
+; Racket interface for apertium-kaz-tat
+;
+; REQUIRES: apertiumpp package.
+; https://taruen.github.io/apertiumpp/apertiumpp/ gives info on how to install
+; it.
+
+(provide kaz-tat-from-pretransfer-to-biltrans
+         kaz-tat-from-t1x-to-postgen)
+
+(require pkg/lib
+         rackunit
+         rash
+         apertiumpp/streamparser)
+
+(define (symbol-append s1 s2)
+  (string->symbol (string-append (symbol->string s1) (symbol->string s2))))
+
+(define A-KAZ-TAT './)
+(define A-KAZ-TAT-BIL (symbol-append A-KAZ-TAT 'kaz-tat.autobil.bin))
+(define A-KAZ-TAT-T1X (symbol-append A-KAZ-TAT 'apertium-kaz-tat.kaz-tat.t1x))
+(define A-KAZ-TAT-T1X-BIN (symbol-append A-KAZ-TAT 'kaz-tat.t1x.bin))
+(define A-KAZ-TAT-T2X (symbol-append A-KAZ-TAT 'apertium-kaz-tat.kaz-tat.t2x))
+(define A-KAZ-TAT-T2X-BIN (symbol-append A-KAZ-TAT 'kaz-tat.t2x.bin))
+(define A-KAZ-TAT-GEN (symbol-append A-KAZ-TAT 'kaz-tat.autogen.bin))
+(define A-KAZ-TAT-PGEN (symbol-append A-KAZ-TAT 'kaz-tat.autopgen.bin))
+
+(define (kaz-tat-from-pretransfer-to-biltrans s)
+  (parameterize ([current-directory (pkg-directory "apertium-kaz-tat")])
+    (rash
+     "echo (values s) | apertium-pretransfer | "
+     "lt-proc -b (values A-KAZ-TAT-BIL)")))
+
+(define (kaz-tat-from-t1x-to-postgen s)
+  (parameterize ([current-directory (pkg-directory "apertium-kaz-tat")])
+    (rash
+     "echo (values s) | "
+     "apertium-transfer -b (values A-KAZ-TAT-T1X) (values A-KAZ-TAT-T1X-BIN) | "
+     "apertium-transfer -n (values A-KAZ-TAT-T2X) (values A-KAZ-TAT-T2X-BIN) | "
+     "lt-proc -g (values A-KAZ-TAT-GEN) | "
+     "lt-proc -p (values A-KAZ-TAT-PGEN)")))
+  
