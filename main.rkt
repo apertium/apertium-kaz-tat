d080e3971c909c052efdb17b37893ca26bc02731
Ilnar Salimzianov <ilnar@selimcan.org>
2019-07-02 09:39:23 +0300

    minor

--- main.rkt
+++ main.rkt
@@ -9,7 +9,9 @@
 (provide kaz-tat
          kaz-tat-from-pretransfer-to-biltrans
          kaz-tat-from-t1x-to-postgen
-         tat-kaz)
+         tat-kaz
+         tat-kaz-from-pretransfer-to-biltrans
+         tat-kaz-from-t1x-to-postgen)
 
 (require pkg/lib
          rackunit
@@ -27,6 +29,13 @@
 (define A-KAZ-TAT-T2X-BIN (symbol-append A-KAZ-TAT 'kaz-tat.t2x.bin))
 (define A-KAZ-TAT-GEN (symbol-append A-KAZ-TAT 'kaz-tat.autogen.bin))
 (define A-KAZ-TAT-PGEN (symbol-append A-KAZ-TAT 'kaz-tat.autopgen.bin))
+(define A-TAT-KAZ-BIL (symbol-append A-KAZ-TAT 'tat-kaz.autobil.bin))
+(define A-TAT-KAZ-T1X (symbol-append A-KAZ-TAT 'apertium-kaz-tat.tat-kaz.t1x))
+(define A-TAT-KAZ-T1X-BIN (symbol-append A-KAZ-TAT 'tat-kaz.t1x.bin))
+(define A-TAT-KAZ-T2X (symbol-append A-KAZ-TAT 'apertium-kaz-tat.tat-kaz.t2x))
+(define A-TAT-KAZ-T2X-BIN (symbol-append A-KAZ-TAT 'tat-kaz.t2x.bin))
+(define A-TAT-KAZ-GEN (symbol-append A-KAZ-TAT 'tat-kaz.autogen.bin))
+(define A-TAT-KAZ-PGEN (symbol-append A-KAZ-TAT 'tat-kaz.autopgen.bin))
 
 (define (kaz-tat s)
   (parameterize ([current-directory (pkg-directory "apertium-kaz-tat")])
@@ -52,3 +61,18 @@
   (parameterize ([current-directory (pkg-directory "apertium-kaz-tat")])
     (rash
      "echo (values s) | apertium -d . tat-kaz")))
+
+(define (tat-kaz-from-pretransfer-to-biltrans s)
+  (parameterize ([current-directory (pkg-directory "apertium-kaz-tat")])
+    (rash
+     "echo (values s) | apertium-pretransfer | "
+     "lt-proc -b (values A-TAT-KAZ-BIL)")))
+
+(define (tat-kaz-from-t1x-to-postgen s)
+  (parameterize ([current-directory (pkg-directory "apertium-kaz-tat")])
+    (rash
+     "echo (values s) | "
+     "apertium-transfer -b (values A-TAT-KAZ-T1X) (values A-TAT-KAZ-T1X-BIN) | "
+     "apertium-transfer -n (values A-TAT-KAZ-T2X) (values A-TAT-KAZ-T2X-BIN) | "
+     "lt-proc -g (values A-TAT-KAZ-GEN) | "
+     "lt-proc -p (values A-TAT-KAZ-PGEN)")))
